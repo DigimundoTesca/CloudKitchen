713f9e70560b8860fdd33ea7be5f98cdca9cca9b
Ramses Martinez <ramses.mtz96@gmail.com>
2016-10-13 06:01:37 -0500

    Lograda la recursion con las metricas

--- supplies/admin.py
+++ supplies/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from .models import Supply, Category, Provider
+from .models import Supply, Category, Provider, StockChain, Metric
 
 @admin.register(Provider)
 class  AdminProvider(admin.ModelAdmin):
-	list_display	= ('name' , 'image')
+    list_display    = ('name' , 'image')
+
 
 @admin.register(Category)
 class  AdminCategory(admin.ModelAdmin):
-	list_display	= ('name' , 'image')
+    list_display    = ('name' , 'image')
+
 
 @admin.register(Supply)
 class  AdminSupply(admin.ModelAdmin):
-	list_display	= ('name' , 'category', 'barcode', 'provider', 'image')
+    list_display    = ('name' , 'category', 'barcode', 'provider', 'ideal_durability', 'image')
+
+
+@admin.register(Metric)
+class  AdminMetric(admin.ModelAdmin):
+    list_display    = ('metric_type', 'stock', 'parent_metric')
+
+
+@admin.register(StockChain)
+class  AdminStockChain(admin.ModelAdmin):
+    list_display    = ('id' , 'registered_at', 'expiry_date', 'supply', 'status', 'metric')
--- supplies/models.py
+++ supplies/models.py
@@ -4,43 +4,91 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.validators import MaxValueValidator
 
+
 class Provider(models.Model):
-	name  = models.CharField(max_length = 255, unique = True)
-	image = models.ImageField(blank = False)	
+    name  = models.CharField(max_length = 255, unique = True)
+    image = models.ImageField(blank = False)    
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		ordering            = ('id',)
-		verbose_name        = 'Provider'
-		verbose_name_plural = 'Providers'
-		
+    class Meta:
+        ordering            = ('id',)
+        verbose_name        = 'Provider'
+        verbose_name_plural = 'Providers'
+        
+
 
 class Category(models.Model):
-	name  = models.CharField(max_length = 255, unique = True)
-	image = models.ImageField(blank = False)	
+    name  = models.CharField(max_length = 255, unique = True)
+    image = models.ImageField(blank = False)    
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		ordering            = ('id',)
-		verbose_name        = 'Category'
-		verbose_name_plural = 'Categories'
+    class Meta:
+        ordering            = ('id',)
+        verbose_name        = 'Category'
+        verbose_name_plural = 'Categories'
+
 
 
 class Supply(models.Model):
-	name     = models.CharField(max_length = 255, unique = True)
-	category = models.ForeignKey(Category, default = 1)
-	barcode  = models.PositiveIntegerField(help_text='(Codigo de barras de 13 digitos)', validators=[MaxValueValidator(9999999999999)], blank = True, null = True)
-	provider = models.ForeignKey(Provider, default = 1)
-	image    = models.ImageField(blank = False)
+    name             = models.CharField(max_length = 255, unique = True)
+    category         = models.ForeignKey(Category, default = 1)
+    barcode          = models.PositiveIntegerField(help_text='(Codigo de barras de 13 digitos)', validators=[MaxValueValidator(9999999999999)], blank = True, null = True)
+    provider         = models.ForeignKey(Provider, default = 1)
+    ideal_durability = models.IntegerField(default=10)
+    image            = models.ImageField(blank = False)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		ordering = ('id',)
-		verbose_name        = 'Supply'
-		verbose_name_plural = 'Supplies'
+    class Meta:
+        ordering            = ('id',)
+        verbose_name        = 'Supply'
+        verbose_name_plural = 'Supplies'
+
+
+
+
+class Metric(models.Model):
+    METRICS_TYPE = (
+        ('gramos', 'gramos'),
+        ('litros', 'litros'),
+        ('empaques', 'empaques'),
+        ('cajas', 'cajas')
+    )
+    metric_type = models.CharField(max_length = 8, choices=METRICS_TYPE)
+    stock       = models.DecimalField(max_digits=9, decimal_places=3)
+    parent_metric   = models.ForeignKey('self', blank = True, null = True)
+
+    def __str__(self):
+        return '%s > %s > %s' % (self.stock, self.metric_type, self.parent_metric)
+
+    class Meta:
+        ordering            = ('id',)
+        verbose_name        = 'Metric'
+        verbose_name_plural = 'Metrics'
+
+
+class StockChain(models.Model):
+    STATUS = (
+        ('1','Provider'),
+        ('2','Stock'),
+        ('3','Assemblied'),
+        ('4','Selled')
+    )
+    registered_at = models.DateField()
+    expiry_date   = models.DateField()
+    supply        = models.ForeignKey(Supply, default = 1)
+    status        = models.CharField(max_length=1, choices=STATUS, default = 1)
+    metric        = models.ForeignKey(Metric, default = 1)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        ordering            = ('id',)
+        verbose_name        = 'Stock Chain'
+        verbose_name_plural = 'Stocks Chain'
