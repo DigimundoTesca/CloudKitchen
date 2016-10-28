f4c5846682981886fd74958bf466f391c8033776
RamsesMartinez <ramses.mtz96@gmail.com>
2016-10-28 18:59:33 -0500

    modificado todo el proyecto para que tenga formato PEP8

--- supplies/forms.py
+++ supplies/forms.py
@@ -3,17 +3,20 @@ from __future__ import unicode_literals
 from django import forms
 from .models import Supply, Category, Provider, Cartridge
 
+
 class SupplyForm(forms.ModelForm):
-	class Meta:
-		model = Supply
-		fields = ('__all__')
+    class Meta:
+        model = Supply
+        fields = '__all__'
+
 
 class CategoryForm(forms.ModelForm):
-	class Meta:
-		model = Category
-		fields = ('__all__')
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 
 class CartridgeForm(forms.ModelForm):
-	class Meta:
-		model = Cartridge
-		fields = ('__all__')
+    class Meta:
+        model = Cartridge
+        fields = '__all__'
--- supplies/urls.py
+++ supplies/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.index, name='index'),
     # Supplies
-    url(r'^login/$', views.login, name = 'views.login'),
+    url(r'^login/$', views.login, name='views.login'),
     url(r'^supplies/$', views.supplies, name='views.supplies'),
     url(r'^supplies/new/$', views.new_supply, name='views.new_supply'),
     url(r'^supplies/(?P<pk>[0-9]+)/$', views.supply_detail, name='views.supply_detail'),
