de8ce75964f8163ccb084f3aedb7336213b658e5
Ramses Martinez <ramses.mtz96@gmail.com>
2016-11-17 04:04:06 -0600

    Reparado el error en la carga del script de livereload. Agregado el icon para las páginas. Reparado el meta viewport

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-	
-})
+    /**LiveReloaded Plugin */
+    // document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
 
-/**LiveReloaded Plugin */
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+})
