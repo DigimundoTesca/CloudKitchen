80e3010690eccbb0bae34154532d3c3f8f30766b
Ramses Martinez <ramses.mtz96@gmail.com>
2016-11-18 01:59:50 -0600

    Terminados los estilos para ventas. Por terminar la carga automatica de nuevos elementos en la lista de compras

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     /**LiveReloaded Plugin */
     // document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
 
-})
+    $(".product-img").click(function () {
+        var id = $(this).siblings('.product-name').text()
+        console.log(id);
+
+    });
+});
