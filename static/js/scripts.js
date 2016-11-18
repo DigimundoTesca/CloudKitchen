af8b27e3e75010afa333254772fb3fd7fb5c0668
Ramses Martinez <ramses.mtz96@gmail.com>
2016-11-18 02:17:29 -0600

    Generacion automatica de nuevos elementos en la lista

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,8 +3,18 @@ $(document).ready(function () {
     // document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
 
     $(".product-img").click(function () {
-        var id = $(this).siblings('.product-name').text()
-        console.log(id);
+        $name = $(this).siblings('.product-name').text()
 
+        $nuevo_li = $("" +
+            "<li class='list-group-item'>" +
+                "<span class='name-li text-uppercase'>"
+                    + $name +
+                "</span> " +
+                "<span class='cant-li'>2</span>" +
+                "<span class='total-li'>" +
+                    "<strong>9.00</strong>" +
+                "</span> " +
+            "</li>");
+        $nuevo_li.appendTo('#sales-list');
     });
 });
