a91cb3b6639609d2663e946cfc6fd8e69d85325c
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-11 13:23:02 -0600

    removido el ordenamiento por id y sustituido por fecha descendente en los tickets

--- supplies/models.py
+++ supplies/models.py
@@ -338,7 +338,7 @@ class Warehouse(models.Model):
 
 
 class Ticket(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, editable=True)
+    created_at = models.DateTimeField()
     seller = models.ForeignKey(UserProfile, default=1, on_delete=models.CASCADE)
     cash_register = models.ForeignKey(CashRegister, on_delete=models.CASCADE, default=1)
 
@@ -346,7 +346,7 @@ class Ticket(models.Model):
         return '%s' % self.id
 
     class Meta:
-        ordering = ('id',)
+        ordering = ('-created_at', )
         verbose_name = 'Ticket '
         verbose_name_plural = 'Tickets'
 
