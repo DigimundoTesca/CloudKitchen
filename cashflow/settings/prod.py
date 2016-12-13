ca6472f16886417bb4f216fa0f6b23f8ad2e624d
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-13 12:02:41 -0600

    Anhadido el host para dabbanet.dabbawala.com.mx

--- cashflow/settings/prod.py
+++ cashflow/settings/prod.py
@@ -2,7 +2,7 @@ from cashflow.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['dabbanet.tescacorporation.com', 'dabbawala.com.mx']
+ALLOWED_HOSTS = ['dabbanet.tescacorporation.com', 'dabbanet.dabbawala.com.mx']
 
 # Database
 DATABASES = {
