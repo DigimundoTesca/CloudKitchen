b88ce85077e0d7aff2b71f591d4e9d42c7ae1818
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-18 13:58:33 -0600

    Reparado el archivo de admin, movidas las vistas a admin en la app users

--- users/views.py
+++ users/views.py
@@ -1,30 +1,3 @@
 from django.shortcuts import render
 
 from users import admin
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-
-
-class UserRolAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(UserRol, UserRolAdmin)
-
-
-class CashRegisterInline(admin.StackedInline):
-    model = CashRegister
-    extra = 2
-
-
-@admin.register(BranchOffice)
-class AdminBranchOffice(admin.ModelAdmin):
-    list_display = ('name', 'manager', 'address',)
-    inlines = [CashRegisterInline, ]
-
-
-@admin.register(Supplier)
-class AdminProvider(admin.ModelAdmin):
-    list_display = ('name', 'image',)
