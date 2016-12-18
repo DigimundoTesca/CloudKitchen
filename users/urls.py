95201c02ca9f0f811c340aea61921b0eb1f74ba5
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-18 14:07:29 -0600

    Reparado el aruchivo de urls de users

--- users/urls.py
+++ users/urls.py
@@ -3,3 +3,13 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'users'
+
+
+urlpatterns = [
+    # auth
+    url(r'^$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+
+    # profile
+    # url(r'^profiles/$', views.ProfileVIew, name='profiles'),
+]
