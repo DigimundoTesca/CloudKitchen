33774fb03bca223c7b41d51e6d49b279b0e573dd
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-01-06 23:06:37 -0600

    Modificados los settings

---
+++ cashflow/settings/prod.py
@@ -0,0 +1,20 @@
+from cashflow.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DABBANET_DB_NAME'),
+        'USER': os.getenv('DABBANET_DB_USER'),
+        'PASSWORD': os.getenv('DABBANET_DB_PASSWORD'),
+        'HOST': os.getenv('DABBANET_DB_HOST'),
+        'PORT': os.getenv('DABBANET_DB_PORT'),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
