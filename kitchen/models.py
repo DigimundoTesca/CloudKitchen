d1cbc6d1d73426ed9c642d6fb47574de688adeca
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-01-21 01:39:40 -0600

    Fixed the kitchen models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import get_object_or_404
 
 from products.models import Supply, Cartridge, PackageCartridge
 from sales.models import Ticket, TicketDetail
@@ -20,36 +19,19 @@ class ProcessedProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     prepared_at = models.DateTimeField(editable=True, null=True, blank=True)
     status = models.CharField(max_length=10, choices=STATUS, default=ASSEMBLED)
-    ticket_detail = models.OneToOneField(TicketDetail, on_delete=models.CASCADE)
-    cartridge_parent = models.ForeignKey(Cartridge, null=True, blank=True)
-    package_parent = models.ForeignKey(PackageCartridge, null=True, blank=True)
-    quantity = models.IntegerField()
+    ticket = models.OneToOneField(Ticket, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % self.created_at
 
-    @receiver(post_save, sender=TicketDetail)
+    @receiver(post_save, sender=Ticket)
     def create_processed_product(sender, instance, **kwargs):
-        ticket_detail = TicketDetail.objects.get(id=instance.id)
+        ticket = Ticket.objects.get(id=instance.id)
         status = 'PE'
-        quantity = instance.quantity
-
-        def get_cartridge():
-            if instance.cartridge:
-                return instance.cartridge
-            return None
-
-        def get_package_cartridge():
-            if instance.package_cartridge:
-                return instance.package_cartridge
-            return None
 
         processed_product = ProcessedProduct.objects.create(
-            ticket_detail=ticket_detail,
+            ticket=ticket,
             status=status,
-            cartridge_parent=get_cartridge(),
-            package_parent=get_package_cartridge(),
-            quantity=quantity,
         )
         processed_product.save()
 
--- kitchen/views.py
+++ kitchen/views.py
@@ -9,14 +9,26 @@ from sales.models import Ticket, TicketDetail
 
 def cold_kitchen(request):
     template = 'kitchen/cold.html'
-    products = ProcessedProduct.objects.filter(status='PE')
     tickets = Ticket.objects.all()
-    ticket_details = TicketDetail.objects.all()
 
+    def get_processed_products():
+        processed_products_list = []
+        processed_product_object = {
+            'ticket_id': None,
+            'cartridges': None,
+            'packages': None,
+        }
+        processed_objects = ProcessedProduct.objects.filter(status='PE')
+        for processed in processed_objects:
+            processed_product_object['ticket_id'] = processed.id
+            print(1)
+        print(processed_product_object)
+        return processed_products_list
+
+    print(get_processed_products())
     context = {
-        'products': products,
+        #'products': products,
         'tickets': tickets,
-        'ticket_details': ticket_details,
         'page_title': PAGE_TITLE,
         'title': 'Cocina Fría'
     }
