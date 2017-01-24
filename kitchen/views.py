9f573287290c2359e7609118a021e693c1cd25d8
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-01-24 12:01:28 -0600

    Added assembly template with styles :)

--- kitchen/views.py
+++ kitchen/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # -------------------------------------  Kitchen -------------------------------------
 from cashflow.settings.base import PAGE_TITLE
 from kitchen.models import ProcessedProduct
-from products.models import PackageCartridgeRecipe
+from products.models import PackageCartridgeRecipe, Cartridge, PackageCartridge
 from sales.models import Ticket, TicketDetail
 
 
@@ -47,13 +47,12 @@ def cold_kitchen(request):
 
             processed_products_list.append(processed_product_object)
         return processed_products_list
-    for xd in get_processed_products():
-        pass
+
     context = {
         'products': get_processed_products(),
         'tickets': tickets,
-        'page_title': PAGE_TITLE,
-        'title': 'Cocina Fría'
+        'page_title': 'Cocina Fría' ,
+        'title': PAGE_TITLE,
     }
 
     return render(request, template, context)
@@ -61,13 +60,92 @@ def cold_kitchen(request):
 
 def hot_kitchen(request):
     template = 'kitchen/hot.html'
-    context = {}
+    tickets = Ticket.objects.all()
+
+    def get_processed_products():
+        processed_products_list = []
+        processed_objects = ProcessedProduct.objects.filter(status='PE')
+
+        for processed in processed_objects:
+            processed_product_object = {
+                'ticket_id': processed.ticket,
+                'cartridges': [],
+                'packages': []
+            }
+
+            for ticket_detail in TicketDetail.objects.filter(ticket=processed.ticket):
+                if ticket_detail.ticket == processed.ticket:
+                    if ticket_detail.cartridge:
+                        cartridge = {
+                            'quantity': ticket_detail.quantity,
+                            'cartridge': ticket_detail.cartridge
+                        }
+                        processed_product_object['cartridges'].append(cartridge)
+
+                    elif ticket_detail.package_cartridge:
+                        package = {
+                            'quantity': ticket_detail.quantity,
+                            'package_recipe': []
+                        }
+                        package_recipe = \
+                            PackageCartridgeRecipe.objects.filter(package_cartridge=ticket_detail.package_cartridge)
+                        for recipe in package_recipe:
+                            package['package_recipe'].append(recipe.cartridge)
+                        processed_product_object['packages'].append(package)
+
+            processed_products_list.append(processed_product_object)
+        return processed_products_list
+
+    context = {
+        'products': get_processed_products(),
+        'tickets': tickets,
+        'page_title': 'Cocina Caliente',
+        'title': PAGE_TITLE,
+    }
 
     return render(request, template, context)
 
 
 def assembly(request):
     template = 'kitchen/assembly.html'
-    context = {}
+
+    pending_orders = ProcessedProduct.objects.filter(status='PE')
+    orders_list = []
+
+    for order in pending_orders:
+        order_object = {
+            'ticket_id': order.ticket,
+            'products': [],
+            'packages': [],
+        }
+
+        ticket_details = TicketDetail.objects.filter(ticket=order.ticket)
+        cartridges = Cartridge.objects.all()
+        packages = PackageCartridge.objects.all()
+
+        for ticket_detail in ticket_details:
+            for cartridge in cartridges:
+                if ticket_detail.cartridge == cartridge:
+                    product_object = {
+                        'name': ticket_detail.cartridge.name,
+                        'quantity': ticket_detail.quantity,
+                    }
+                    order_object['products'].append(product_object)
+
+            for package in packages:
+                if ticket_detail.package_cartridge == package:
+                    package_object = {
+                        'name': ticket_detail.package_cartridge.name,
+                        'quantity': ticket_detail.quantity,
+                    }
+                    order_object['packages'].append(package_object)
+
+        orders_list.append(order_object)
+
+    context = {
+        'orders': orders_list,
+        'page_title': 'Ensamblado',
+        'title': PAGE_TITLE
+    }
 
     return render(request, template, context)
