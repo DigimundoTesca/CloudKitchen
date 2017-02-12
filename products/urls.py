4262ca4e09ef3e132701afa90b53bf781ea77c08
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-02-11 20:46:18 -0600

    Added cartridges details logic

--- products/urls.py
+++ products/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     url(r'^supplies/new/$', views.new_supply, name='new_supply'),
     url(r'^supplies/(?P<pk>[0-9]+)/$', views.supply_detail, name='supply_detail'),
 
+    # Cartridges
+    url(r'^cartridges/$', views.cartridges, name='cartridges'),
+    url(r'^cartridges/new/$', views.new_cartridge, name='new_cartridge'),
+    url(r'^cartridges/(?P<pk>[0-9]+)/$', views.cartridge_detail, name='cartridge_detail'),
+
     # Suppliers
     url(r'^suppliers/$', views.suppliers, name='suppliers'),
 
@@ -18,10 +23,6 @@ urlpatterns = [
     url(r'^categories/$', views.categories, name='categories'),
     url(r'^categories/new/$', views.new_category, name='new_category'),
     url(r'^categories/([A-Za-z]+)/$', views.categories_supplies, name='categories_supplies'),
-
-    # Categories
-    url(r'^cartridges/$', views.cartridges, name='cartridges'),
-    url(r'^cartridges/new/$', views.new_cartridge, name='new_cartridge'),
 ]
 
 # test
